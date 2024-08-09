28a04783d850e8a82072a168cd42d24a9f16b63c
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-08-09 13:38:45 +0000

    update views and url files and deployment settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'xNU4xxSAFR'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-fernvr-djangoblog-ogldj6vf4gp.ws.codeinstitute-ide.net','.herokuapp.com']
 
