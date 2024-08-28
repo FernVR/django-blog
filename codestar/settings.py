508329b3f0d07a1360c8ebbe887d781f4787c44c
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-08-28 13:40:29 +0000

    deployment settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'xNU4xxSAFR'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-fernvr-djangoblog-ogldj6vf4gp.ws.codeinstitute-ide.net','.herokuapp.com']
 
