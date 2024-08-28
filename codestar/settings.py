b9fac67885205f07e090d7558abda2ab6431f642
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-08-28 13:35:06 +0000

    add about app folder and wire up models and urls

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'xNU4xxSAFR'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-fernvr-djangoblog-ogldj6vf4gp.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
